18b4bbcb214542b97dbbdb9e147f28874684d0f3
Aswin Kumar S <kumaraswins@gmail.com>
2021-02-02 22:48:41 +0530

    reactive form validation

--- src/app/hero-features/hero-features.component.ts
+++ src/app/hero-features/hero-features.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-features',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroFeaturesComponent implements OnInit {
 
-  constructor() { }
+  registerForm = new FormGroup({
+    name: new FormControl('',[ Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    username: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(10) ]),
+    password: new FormControl('',Validators.required),
+    confirmPassword: new FormControl('',Validators.required),
+  } );
 
-  ngOnInit(): void {
+  constructor() { }
+  ngOnInit(): void {}
+
+  get name(){return this.registerForm.get("name")}
+  get email(){return this.registerForm.get("email")}
+  get username(){return this.registerForm.get("username")}
+  get password(){return this.registerForm.get("password")}
+  get confirmPassword(){return this.registerForm.get("confirmPassword")}
+
+  // get registerFormControl() {
+  //   return this.registerForm?.controls;
+  // }
+  onSubmit() {
+
   }
 
 }
